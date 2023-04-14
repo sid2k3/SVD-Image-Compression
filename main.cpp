ab3b48e5d25c32dbd5c305c8237f130a19b7e3ba
sid2k3 <siddharth23kataria@gmail.com>
2023-04-15 03:05:31 +0530

    Refactored main.cpp

--- main.cpp
+++ main.cpp
@@ -9,12 +9,11 @@
 #include <emscripten/bind.h>
 
 using namespace emscripten;
-using namespace std;
 
 Eigen::MatrixXd get_compressed_image(Eigen::MatrixXd &mat, int rank, char component)
 {
 
-    RedSVD::RedSVD mtr(mat, rank);
+    RedSVD::RedSVD<Eigen::MatrixXd> mtr(mat, rank);
 
     Eigen::MatrixXd U = mtr.matrixU();
 
@@ -23,14 +22,14 @@ Eigen::MatrixXd get_compressed_image(Eigen::MatrixXd &mat, int rank, char compon
     V.transposeInPlace();
 
     Eigen::MatrixXd compressed = U * S * V;
-    cout << component << " component compressed" << endl;
+    std::cout << component << " component compressed" << std::endl;
     return compressed;
 }
 
-vector<vector<double>> get_image_matrix(int width, int length)
+std::vector<std::vector<double>> get_image_matrix(int width, int length)
 {
 
-    vector<int> pixel_sequence;
+    std::vector<int> pixel_sequence;
 
     std::ifstream myfile("mat.txt");
 
@@ -40,9 +39,9 @@ vector<vector<double>> get_image_matrix(int width, int length)
         pixel_sequence.push_back(temp);
     }
 
-    vector<double> img_r;
-    vector<double> img_g;
-    vector<double> img_b;
+    std::vector<double> img_r;
+    std::vector<double> img_g;
+    std::vector<double> img_b;
 
     int cur_pos = 0;
     while (img_r.size() != length * width)
@@ -68,11 +67,11 @@ vector<vector<double>> get_image_matrix(int width, int length)
     }
     return {img_r, img_g, img_b};
 }
-void reconstruct_image(Eigen::MatrixXd &img_r, Eigen::MatrixXd &img_g, Eigen::MatrixXd &img_b)
+std::vector<int> reconstruct_image(Eigen::MatrixXd &img_r, Eigen::MatrixXd &img_g, Eigen::MatrixXd &img_b)
 {
-    vector<double> r_vec;
-    vector<double> g_vec;
-    vector<double> b_vec;
+    std::vector<double> r_vec;
+    std::vector<double> g_vec;
+    std::vector<double> b_vec;
 
     for (auto &x : img_r.reshaped())
     {
@@ -103,7 +102,7 @@ void reconstruct_image(Eigen::MatrixXd &img_r, Eigen::MatrixXd &img_g, Eigen::Ma
         b_vec.push_back(x);
     }
 
-    vector<int> final_img;
+    std::vector<int> final_img;
     for (int i{0}; i < r_vec.size(); i++)
     {
 
@@ -111,62 +110,77 @@ void reconstruct_image(Eigen::MatrixXd &img_r, Eigen::MatrixXd &img_g, Eigen::Ma
         final_img.push_back(lround(g_vec[i]));
         final_img.push_back(lround(b_vec[i]));
     }
-    std::ofstream output_file("compressed_mat.txt");
-
-    std::ostream_iterator<int> output_iterator(output_file, " ");
-    std::copy(std::begin(final_img), std::end(final_img), output_iterator);
+    return final_img;
 }
 
 typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> MyMatrix;
 
-int main(int argc, char **argv)
+std::vector<int> get_compressed_img(int len, int wd, int rank, std::vector<double> &r_vec, std::vector<double> &g_vec, std::vector<double> &b_vec)
 {
+    // std::cout << len << " -> " << wd << std::endl;
 
-    int len, wd;
-    len = atoi(argv[1]);
-    wd = atoi(argv[2]);
-    cout << len << " -> " << wd << endl;
+    // std::vector<std::vector<double>> rgb_mat = get_image_matrix(len, wd);
+    double *r_ptr = r_vec.data();
+    double *g_ptr = g_vec.data();
+    double *b_ptr = b_vec.data();
 
-    vector<vector<double>> rgb_mat = get_image_matrix(len, wd);
-    double *r_vec = rgb_mat[0].data();
-    double *g_vec = rgb_mat[1].data();
-    double *b_vec = rgb_mat[2].data();
-
+    std::cout << "ok" << std::endl;
     size_t nrow = len;
     size_t ncol = wd;
-    MyMatrix img_r = Eigen::Map<MyMatrix>(rgb_mat[0].data(), nrow, ncol);
+    MyMatrix img_r = Eigen::Map<MyMatrix>(r_ptr, nrow, ncol);
 
-    MyMatrix img_g = Eigen::Map<MyMatrix>(rgb_mat[1].data(), nrow, ncol);
-    MyMatrix img_b = Eigen::Map<MyMatrix>(rgb_mat[2].data(), nrow, ncol);
+    MyMatrix img_g = Eigen::Map<MyMatrix>(g_ptr, nrow, ncol);
+    MyMatrix img_b = Eigen::Map<MyMatrix>(b_ptr, nrow, ncol);
 
-    cout << "STEP 1 DONE" << endl;
-    int rank;
-    cout << "Enter Rank:" << endl;
-    cin >> rank;
+    std::cout << "STEP 1 DONE" << std::endl;
 
-    // cout << "DONE" << endl;
+    // std::future<Eigen::MatrixXd> f1 = std::async(std::launch::async, get_compressed_image, std::ref(img_r), rank, 'R');
 
-    future<Eigen::MatrixXd> f1 = async(std::launch::async, get_compressed_image, std::ref(img_r), rank, 'R');
+    // std::future<Eigen::MatrixXd> f2 = std::async(std::launch::async, get_compressed_image, std::ref(img_g), rank, 'G');
+    // std::future<Eigen::MatrixXd> f3 = std::async(std::launch::async, get_compressed_image, std::ref(img_b), rank, 'B');
+    std::cout << "threads created" << std::endl;
+    // Eigen::
+    //     MatrixXd compressed_img_r = f1.get();
 
-    future<Eigen::MatrixXd> f2 = async(std::launch::async, get_compressed_image, std::ref(img_g), rank, 'G');
-    future<Eigen::MatrixXd> f3 = async(std::launch::async, get_compressed_image, std::ref(img_b), rank, 'B');
+    // Eigen::
+    //     MatrixXd compressed_img_g = f2.get();
 
-    Eigen::
-        MatrixXd compressed_img_r = f1.get();
+    // Eigen::
+    //     MatrixXd compressed_img_b = f3.get();
 
-    Eigen::
-        MatrixXd compressed_img_g = f2.get();
+    Eigen::MatrixXd compressed_img_r = get_compressed_image(img_r, rank, 'R');
+    Eigen::MatrixXd compressed_img_g = get_compressed_image(img_g, rank, 'G');
+    Eigen::MatrixXd compressed_img_b = get_compressed_image(img_b, rank, 'B');
+    std::cout
+        << "STEP 2 DONE" << std::endl;
 
-    Eigen::
-        MatrixXd compressed_img_b = f3.get();
-
-    cout << "STEP 2 DONE" << endl;
-
-    reconstruct_image(compressed_img_r, compressed_img_g, compressed_img_b);
+    return reconstruct_image(compressed_img_r, compressed_img_g, compressed_img_b);
 }
 
 EMSCRIPTEN_BINDINGS(my_module)
 {
     register_vector<int>("VectorInt");
+    register_vector<double>("VectorDouble");
     register_vector<std::vector<int>>("VectorVectorInt");
+    function("get_compressed_img", &get_compressed_img);
 }
+
+// int main()
+// {
+//     std::vector<double> vr;
+//     std::vector<double> vg;
+//     std::vector<double> vb;
+
+//     for (int i{0}; i < 9; i++)
+//     {
+//         vr.push_back(10);
+//         vg.push_back(40);
+//         vb.push_back(240);
+//     }
+
+//     std::vector<int> temp = get_compressed_img(3, 3, 1, vr, vg, vb);
+//     for (auto &e : temp)
+//         std::cout << e << " ";
+
+//     std::cout << std::endl;
+// }
